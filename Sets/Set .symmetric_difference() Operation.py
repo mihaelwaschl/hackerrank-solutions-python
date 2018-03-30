ab9e8e54564164118b8f73a5b24907afec70b6bf
MACBOOK\Mihael1 <mihael.waschl@gmail.com>
2018-03-30 20:11:50 +0200

    1

---
+++ Sets/Set .symmetric_difference() Operation.py
@@ -0,0 +1,6 @@
+numE = int(input())
+english = set(map(int, input().strip().split()))
+numF = int(input())
+french = set(map(int, input().strip().split()))
+
+print(len(english.symmetric_difference(french)))
