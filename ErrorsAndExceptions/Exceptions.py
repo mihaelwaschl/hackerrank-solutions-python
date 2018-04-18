cd2e2b992ee032a03379275db4a77e2deaabd45a
Mihael <mihael.waschl@gmail.com>
2018-04-18 20:33:02 +0200

    commit1

---
+++ ErrorsAndExceptions/Exceptions.py
@@ -0,0 +1,9 @@
+n = int(input())
+for i in range(0,n):
+    a, b = input().split()
+    try:
+        print(int(a)//int(b))
+    except ZeroDivisionError as e:
+        print('Error Code:', e)
+    except ValueError as f:
+        print('Error Code:', f)
