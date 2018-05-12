e7d8485f756893b52ea71a42ab6fcbee2ab82d27
Mihael <mihael.waschl@gmail.com>
2018-05-12 16:32:02 +0200

    commit1

--- Collections/Collections.OrderedDict().py
+++ Collections/Collections.OrderedDict().py
@@ -4,10 +4,11 @@ ordered_dictionary = OrderedDict()
 num_item = int(input())
 
 for i in range(0,num_item):
-    item, price = input().split()
+    item, price = input().rsplit(' ', 1)
     if item in ordered_dictionary:
         ordered_dictionary[item] += int(price)
     else:
         ordered_dictionary[item] = int(price)
 
-print(ordered_dictionary)
+for j in ordered_dictionary:
+    print(j +' '+ str(ordered_dictionary[j]))
