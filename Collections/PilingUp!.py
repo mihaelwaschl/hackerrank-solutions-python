629cd97e682980ba7062ff51d2ad68f6b6088903
Mihael <mihael.waschl@gmail.com>
2018-05-15 21:27:58 +0200

    commit1

--- Collections/PilingUp!.py
+++ Collections/PilingUp!.py
@@ -1,34 +1,23 @@
 from collections import deque
 
-start = deque()
-finish = deque()
 result = 'Yes'
 numTestCases = int(input())
 
 for i in range(numTestCases):
     numCubes = int(input())
-    sideLength = map(int, input().split())
-    for cube in sideLength:
-        start.append(cube)
+    start = deque(map(int, input().split()))
+    base = max(start[0], start[-1])
+
     while len(start) > 0:
         if int(start[0]) < int(start[-1]):
-            finish.append(start[-1])
-            start.pop()
-        elif int(start[0]) > int(start[-1]):
-            finish.append(start[0])
-            start.popleft()
-        elif int(start[0]) == int(start[-1]):
-            finish.append(start[0])
-            start.popleft()
-    for j in range(1,len(finish)):
-       if finish[j-1] < finish[j]:
-           result = 'No'
-           print(finish[j-2])
-           print(finish[j - 1])
-           print(finish[j])
-           print(finish[j + 1])
-           print(finish[j + 2])
-           break
-    start.clear()
-    finish.clear()
-    print(result)
+            item =start.pop()
+        else:
+            item = start.popleft()
+        if item > base:
+            print('No')
+            break
+        else:
+            base = item
+    else:
+        print('Yes')
+        start.clear()
