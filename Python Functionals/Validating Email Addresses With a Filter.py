db775a66c5a36148838680f26d8647beae7d5c19
Mihael <mihael.waschl@gmail.com>
2018-07-10 00:00:29 +0200

    commit1

--- Python Functionals/Validating Email Addresses With a Filter.py
+++ Python Functionals/Validating Email Addresses With a Filter.py
@@ -1,4 +1,16 @@
-def fun(s):
-    
+import re
 
+n = int(input())
+allmails = ''
+for i in range(n):
+    mail = str(input())
+    allmails += mail +'\n'
 
+pattern = re.compile(r'[a-zA-Z0-9\.-]+@[a-zA-Z0-9]+\.\w{3}')
+finded = pattern.findall(allmails)
+
+result = []
+for match in finded:
+    result.append(match)
+
+print(result)
