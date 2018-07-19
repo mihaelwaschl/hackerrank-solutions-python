f7c1e73169d766b3f9393b0d0c5bbfd899d478b1
Mihael <mihael.waschl@gmail.com>
2018-07-19 15:44:00 +0200

    commit1

--- Python Functionals/Validating Email Addresses With a Filter.py
+++ Python Functionals/Validating Email Addresses With a Filter.py
@@ -3,6 +3,6 @@ import re
 all_mails = []
 for i in range(int(input())):
     all_mails.append(input())
-result = list(filter(lambda x: re.search(r'^[a-z0-9_-]+@+[a-z0-9]+\.\w{1,3}$', x , re.IGNORECASE), all_mails))
+result = list(filter(lambda x: re.search(r'^[a-z0-9_-]+@[a-z0-9]+\.\w{1,3}$', x , re.IGNORECASE), all_mails))
 print(sorted(result))
 
