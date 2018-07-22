6b862686ebe7071c6f077e4f6dc85edae883a950
Mihael <mihael.waschl@gmail.com>
2018-07-22 19:05:44 +0200

    commit1

--- XML/XML 1 - Find the Score.py
+++ XML/XML 1 - Find the Score.py
@@ -3,8 +3,7 @@ import xml.etree.ElementTree as ET
 n_line = int(input())
 xml = '\n'.join(input() for i in range(n_line))
 root = ET.fromstring(xml)
-num_attr = 0
-num_attr += len(root.attrib)
+num_attr = len(root.attrib)
 for child in root:
     num_attr += len(child.attrib)
 print(num_attr)
