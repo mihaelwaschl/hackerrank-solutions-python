c9d5e9a944ae5d04a96d1e09f1fc6f99f0abee0b
Mihael <mihael.waschl@gmail.com>
2018-07-22 23:54:32 +0200

    commit1

--- Regex/Array Mathematics.py
+++ Regex/Array Mathematics.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 n, m = map(int, input().split())
-a = np.array(input().split(), int)
-b = np.array(input().split(), int)
+a = np.array([input().split() for _ in range(n)], int)
+b = np.array([input().split() for _ in range(n)], int)
 print(a+b)
 print(a-b)
 print(a*b)
